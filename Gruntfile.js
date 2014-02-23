0585f61527a369c4d9e8498a86b650de5fa24320
benestar <benestar.wikimedia@gmail.com>
2014-02-23 13:56:56 +0000

    add jslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,20 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 
 	grunt.initConfig( {
 		qunit: {
 			all: [ 'test/qunit.html' ]
 		},
+		jshint: {
+			all: [ 'Gruntfile.js', 'src/*.json', 'test/test_*.json' ]
+		},
 		jsonlint: {
-			all: [ 'i18n/*.json', 'patterns/*.json' ]
+			all: [ 'package.json', 'i18n/*.json', 'patterns/*.json' ]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'qunit', 'jsonlint' ] );
+	grunt.registerTask( 'test', [ 'qunit', 'jshint', 'jsonlint' ] );
 	grunt.registerTask( 'default', [ 'test' ] );
 };
