4dab69ecb7aacc580b0c5f21175fb7b527f8716c
Benestar <benestar.wikimedia@googlemail.com>
2014-03-04 10:26:42 +0100

    Fetch the expected results using getJSON
    This fixes the problem that the tests fail when the data is changed.

--- test/test_api.js
+++ test/test_api.js
@@ -26,26 +26,27 @@ asyncTest( '_get', function() {
 	.always( start );
 } );
 
-// @todo: verify that search entities etc. works. Note that the data may change so we have to be careful at this point.
-// Note: perhaps it would be best to query the data using getJSON on our own, too, but this does not really test something.
-
 asyncTest( 'format datavalue', function() {
 	expect( 2 );
 
 	var api = new Api( 'https://www.wikidata.org/w/api.php', 'en' );
-	$.when( api.formatDatavalue( {"value":{"time":"+00000002001-06-16T00:00:00Z","timezone":0,"before":0,"after":0,"precision":11,"calendarmodel":"http://www.wikidata.org/entity/Q1985727"},"type":"time"} ) )
-	.then( function( data ) {
+	var datavalue1 = {"value":{"time":"+00000002001-06-16T00:00:00Z","timezone":0,"before":0,"after":0,"precision":11,"calendarmodel":"http://www.wikidata.org/entity/Q1985727"},"type":"time"};
+	var datavalue2 = {"value":{"entity-type":"item","numeric-id":1208},"type":"wikibase-entityid"};
+	$.when(
+		api.formatDatavalue( datavalue1 ),
+		$.getJSON( 'https://www.wikidata.org/w/api.php?action=wbformatvalue&generate=text%2Fplain&datavalue=' + encodeURIComponent( datavalue1 ) + '&options=%7B%22lang%22%3A%22en%22%2C%22geoformat%22%3A%22dms%22%7D&format=json&callback=?' ),
+		api.formatDatavalue( datavalue2 ),
+		$.getJSON( 'https://www.wikidata.org/w/api.php?action=wbformatvalue&generate=text%2Fplain&datavalue=' + encodeURIComponent( datavalue2 ) + '&options=%7B%22lang%22%3A%22en%22%2C%22geoformat%22%3A%22dms%22%7D&format=json&callback=?')
+	)
+	.then( function( data1, expected1, data2, expected2 ) {
 		deepEqual(
-			data,
-			{"warnings":{"main":{"*":"Unrecognized parameter: '_'"}},"result":"16 June 2001"},
+			data1[0],
+			expected1[0],
 			'time data value'
 		);
-		return api.formatDatavalue( {"value":{"entity-type":"item","numeric-id":1208},"type":"wikibase-entityid"} );
-	} )
-	.then( function( data ) {
 		deepEqual(
-			data,
-			{"warnings":{"main":{"*":"Unrecognized parameter: '_'"}},"result":"Brandenburg"},
+			data2[0],
+			expected2[0],
 			'entity id data value'
 		);
 	} )
@@ -56,11 +57,14 @@ asyncTest( 'get claims', function() {
 	expect( 1 );
 
 	var api = new Api( 'https://www.wikidata.org/w/api.php', 'en' );
-	api.getClaims( 'q76', 'p21' )
-	.done( function( data ) {
+	$.when(
+		api.getClaims( 'q76', 'p21' ),
+		$.getJSON( 'https://www.wikidata.org/w/api.php?action=wbgetclaims&entity=q76&property=p21&format=json&callback=?' )
+	)
+	.then( function( data1, expected1 ) {
 		deepEqual(
-			data,
-			{"warnings":{"main":{"*":"Unrecognized parameter: '_'"}},"claims":{"P21":[{"id":"q76$545B2D30-AD63-4698-9224-DDB75CC685FC","mainsnak":{"snaktype":"value","property":"P21","datavalue":{"value":{"entity-type":"item","numeric-id":6581097},"type":"wikibase-entityid"}},"type":"statement","rank":"normal","references":[{"hash":"a51d6594fee36c7452eaed2db35a4833613a7078","snaks":{"P143":[{"snaktype":"value","property":"P143","datavalue":{"value":{"entity-type":"item","numeric-id":54919},"type":"wikibase-entityid"}}]},"snaks-order":["P143"]},{"hash":"0d477c3857899469474d86574114856e00d9933c","snaks":{"P248":[{"snaktype":"value","property":"P248","datavalue":{"value":{"entity-type":"item","numeric-id":14527788},"type":"wikibase-entityid"}}]},"snaks-order":["P248"]}]}]}},
+			data1[0],
+			expected1[0],
 			'Checking result'
 		);
 	} )
@@ -71,11 +75,14 @@ asyncTest( 'get entities', function() {
 	expect( 1 );
 
 	var api = new Api( 'https://www.wikidata.org/w/api.php', 'en' );
-	api.getEntities( 'q76', 'labels' )
-	.done( function( data ) {
+	$.when(
+		api.getEntities( 'q76', 'labels' ),
+		$.getJSON( 'https://www.wikidata.org/w/api.php?action=wbgetentities&languages=en&ids=q76&props=labels&format=json&callback=?' )
+	)
+	.then( function( data1, expected1 ) {
 		deepEqual(
-			data,
-			{"warnings":{"main":{"*":"Unrecognized parameter: '_'"}},"entities":{"Q76":{"id":"Q76","type":"item","labels":{"en":{"language":"en","value":"Barack Obama"}}}},"success":1},
+			data1[0],
+			expected1[0],
 			'Checking result'
 		);
 	} )
@@ -86,16 +93,20 @@ asyncTest( 'search entities', function() {
 	expect( 2 );
 
 	var api = new Api( 'https://www.wikidata.org/w/api.php', 'en' );
-	$.when( api.searchEntities( 'item', 'United States' ), api.searchEntities( 'property', 'location' ) )
-	.done( function( data1, data2 ) {
+	$.when(
+		api.searchEntities( 'item', 'United States' ),
+		$.getJSON( 'https://www.wikidata.org/w/api.php?action=wbsearchentities&language=en&search=United+States&type=item&format=json&callback=?' ),
+		api.searchEntities( 'property', 'location' ) ),
+		$.getJSON( 'https://www.wikidata.org/w/api.php?action=wbsearchentities&language=en&search=location&type=property&format=json&callback=?' )
+	.done( function( data1, expected1, data2, expected2 ) {
 		deepEqual(
 			data1[0],
-			{"warnings":{"main":{"*":"Unrecognized parameter: '_'"}},"searchinfo":{"search":"United States"},"search":[{"id":"Q30","url":"//www.wikidata.org/wiki/Q30","description":"country in North America","label":"United States of America","aliases":["United States"]},{"id":"Q232865","url":"//www.wikidata.org/wiki/Q232865","description":"Wikipedia disambiguation page","label":"United States"},{"id":"Q3965121","url":"//www.wikidata.org/wiki/Q3965121","description":"television series","label":"United States"},{"id":"Q4917","url":"//www.wikidata.org/wiki/Q4917","description":"currency of the United States of America","label":"United States dollar"},{"id":"Q11703","url":"//www.wikidata.org/wiki/Q11703","description":"group of islands in the Caribbean","label":"Virgin Islands of the United States","aliases":["United States Virgin Islands"]},{"id":"Q35657","url":"//www.wikidata.org/wiki/Q35657","description":"constituent political entity sharing sovereignty with the United States of America","label":"U.S. state","aliases":["United States state","United States statehood"]},{"id":"Q29552","url":"//www.wikidata.org/wiki/Q29552","description":"political party in the United States","label":"Democratic Party","aliases":["United States Democratic Party"]}],"search-continue":7,"success":1},
+			expected1[0],
 			'Checking result'
 		);
 		deepEqual(
 			data2[0],
-			{"warnings":{"main":{"*":"Unrecognized parameter: '_'"}},"searchinfo":{"search":"location"},"search":[{"id":"P706","url":"//www.wikidata.org/wiki/Property:P706","description":"located on the specified landform or body of water. Should not be used when the value is only political/administrative (towns, cities, provinces, states, countries, etc.).","label":"located on terrain feature","aliases":["location"]},{"id":"P625","url":"//www.wikidata.org/wiki/Property:P625","description":"coordinates of the subject","label":"coordinate location","aliases":["location"]},{"id":"P766","url":"//www.wikidata.org/wiki/Property:P766","description":"place where a specific event took (or will take) place","label":"event location","aliases":["location"]},{"id":"P189","url":"//www.wikidata.org/wiki/Property:P189","description":"where the item was located when discovered","label":"discovery place","aliases":["location of discovered object"]},{"id":"P276","url":"//www.wikidata.org/wiki/Property:P276","description":"location of a moveable object","label":"moveable object location","aliases":["location of moveable object"]},{"id":"P19","url":"//www.wikidata.org/wiki/Property:P19","description":"the most specific known (e.g. city instead of country)","label":"place of birth","aliases":["location born"]}],"success":1},
+			expected2[0],
 			'Checking result'
 		);
 	} )
